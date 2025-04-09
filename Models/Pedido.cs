b9b9f40a609ccbf0c65175146c8ec90cb15a6101
FIC <fic@info.local>
2025-04-08 21:55:04 -0300

    API

---
+++ Models/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceAPI.Models;
+
+public partial class Pedido
+{
+    public int IdPedido { get; set; }
+
+    public DateOnly? DataPedido { get; set; }
+
+    public string? Status { get; set; }
+
+    public decimal? ValorTotal { get; set; }
+
+    public int? IdCliente { get; set; }
+
+    public virtual Cliente? IdClienteNavigation { get; set; }
+
+    public virtual ICollection<Itempedido> Itempedidos { get; set; } = new List<Itempedido>();
+
+    public virtual ICollection<Pagamento> Pagamentos { get; set; } = new List<Pagamento>();
+}
